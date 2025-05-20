2fcf6fabcc84d4e0fa0e7c8e7b8a7b7e17fc60e8
baptiste gresse <baptiste5gresse@gmail.com>
2025-05-20 18:03:15 +0200

    feat: integrate Solana Mobile Wallet Adapter and update dependencies in package.json and bun.lock

--- src/components/providers/WalletProvider.tsx
+++ src/components/providers/WalletProvider.tsx
@@ -9,10 +9,13 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 
-// Default styles that can be overridden by your app
-import "@solana/wallet-adapter-react-ui/styles.css";
+import {
+  PhantomWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
 
-// imports here
+import { SolanaMobileWalletAdapter } from "@solana-mobile/wallet-adapter-mobile";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function AppWalletProvider({
   children,
@@ -21,10 +24,27 @@ export default function AppWalletProvider({
 }) {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
   const wallets = useMemo(
     () => [
+      new PhantomWalletAdapter(),
+      new SolanaMobileWalletAdapter({
+        appIdentity: {
+          name: "TonNomDApp",
+        },
+        addressSelector: {
+          select: () => Promise.resolve("44'/501'/0'/0/0"),
+        },
+        authorizationResultCache: {
+          get: async () => undefined,
+          set: async () => { },
+          clear: async () => { },
+        },
+        chain: WalletAdapterNetwork.Devnet,
+        onWalletNotFound: async () => { },
+      }),
     ],
-    [],
+    []
   );
 
   return (
