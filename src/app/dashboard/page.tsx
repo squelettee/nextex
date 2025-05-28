06a038ce9eae2d37c8460fad8ae259e304d5160f
baptiste gresse <baptiste5gresse@gmail.com>
2025-05-28 23:28:25 +0200

    fix: implement wallet validation and redirect logic in DashboardPage to enhance user experience

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,23 @@
 import getUser from "@/query/getUser";
 import getUsers from "@/query/getUsers";
+import { redirect } from "next/navigation";
 import DashboardClient from "./DashboardClient";
 
 
 export default async function DashboardPage({ searchParams }: { searchParams: Promise<{ wallet?: string }> }) {
   const users = await getUsers();
   const { wallet } = await searchParams;
-  if (!wallet) return <div>No wallet provided</div>;
+
+  if (!wallet) {
+    redirect("/");
+  }
+
   const user = await getUser(wallet);
+
+  if (!user) {
+    console.log("User not found for wallet:", wallet);
+    redirect("/");
+  }
 
   return <DashboardClient usersProps={users} user={user} />;
 }
